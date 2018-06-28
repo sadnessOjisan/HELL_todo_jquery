9a0f797d1ae6c75ea53e28f28d8705dbd1edd24d
sadness_ojisan <sadness.ojisan@gmail.com>
2018-06-29 02:30:08 +0900

    [udpate] check数を表示

--- main.js
+++ main.js
@@ -7,13 +7,24 @@ $(document).ready(function () {
                 url: "https://json-now-ohjoczewvz.now.sh/todos",
                 data: {
                     'task': todo,
+                    isDone: false
                 }
             })
             .done((data) => {
-                alert('success!')
-                console.log(data);
+                $('#todos-area').append('<input type="checkbox" class="todo-check" /><span>' + data.task + '</span><br />')
+                $('#remain').text($('#todos-area input:checkbox').length-$('#todos-area input:checkbox:checked').length)
+                $('.todo-check').on('click', function(e){
+                    var isChecked = $(this).is(':checked')
+                    $('#remain').text($('#todos-area input:checkbox').length-$('#todos-area input:checkbox:checked').length)
+                })
             }).fail((data) => {
                 alert('fail!')
             });
     });
+
+    $('#remain').text($('#todos-area input:checkbox').length-$('#todos-area input:checkbox:checked').length)
+
+    $('.todo-check').on('click', function(e){
+        console.log(e)
+    })
 });
