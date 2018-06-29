0546a3a4a6926921e7b8b79458c90778efc8e9c0
sadness_ojisan <sadness.ojisan@gmail.com>
2018-06-30 00:15:47 +0900

    [update] add style in toggle

--- main.js
+++ main.js
@@ -11,11 +11,21 @@ $(document).ready(function () {
                 }
             })
             .done((data) => {
-                $('#todos-area').append('<input type="checkbox" class="todo-check" /><span>' + data.task + '</span><br />')
+                $('#todos-area').append('<input type="checkbox" class="todo-check unchecked" id=todo_' + data.id + ' /><span>' + data.task + '</span><br />')
                 $('#remain').text($('#todos-area input:checkbox').length-$('#todos-area input:checkbox:checked').length)
                 $('.todo-check').on('click', function(e){
                     var isChecked = $(this).is(':checked')
+                    var clickedId = $(this).attr('id')
+                    console.log(clickedId)
                     $('#remain').text($('#todos-area input:checkbox').length-$('#todos-area input:checkbox:checked').length)
+                    var todoDom = $("#" + clickedId).parent()
+                    if(isChecked){
+                        todoDom.removeClass("unchecked")
+                        todoDom.addClass("checked")
+                    }else{
+                        todoDom.removeClass("checked")
+                        todoDom.addClass("unchecked")
+                    }
                 })
             }).fail((data) => {
                 alert('fail!')
