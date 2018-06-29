4bc45b1c496ae3d018d83327a769bcde709226bf
sadness_ojisan <sadness.ojisan@gmail.com>
2018-06-30 00:37:43 +0900

    [update] 初期化処理完了

--- main.js
+++ main.js
@@ -1,4 +1,36 @@
 $(document).ready(function () {
+    // 初期化処理
+    $.ajax({
+        type: "GET",
+        url: "https://json-now-ohjoczewvz.now.sh/todos"
+    })
+    .done((data) => {
+        console.log(data)
+        for(var i=0; i<data.length; i++){
+            var todo = data[i]
+            $('#todos-area').append('<p class="unchecked"><input type="checkbox" class="todo-check" id=todo_' + todo.id + ' /><span>' + todo.task + '</span></p>')      
+        }
+        $('#remain').text($('#todos-area input:checkbox').length-$('#todos-area input:checkbox:checked').length)
+        $('.todo-check').on('click', function(e){
+            var isChecked = $(this).is(':checked')
+            var clickedId = $(this).attr('id')
+            console.log(clickedId)
+            $('#remain').text($('#todos-area input:checkbox').length-$('#todos-area input:checkbox:checked').length)
+            var todoDom = $("#" + clickedId).parent()
+            console.log(todoDom)
+            if(isChecked){
+                todoDom.removeClass("unchecked")
+                todoDom.addClass("checked")
+            }else{
+                todoDom.removeClass("checked")
+                todoDom.addClass("unchecked")
+            }
+        })
+    }).fail((data) => {
+        alert('fail!')
+    });
+
+    // todo 追加処理
     $('#submit-form').submit(function (event) {
         event.preventDefault();
         var todo = $('#submit-form [name=todo]').val();
@@ -11,7 +43,7 @@ $(document).ready(function () {
                 }
             })
             .done((data) => {
-                $('#todos-area').append('<input type="checkbox" class="todo-check unchecked" id=todo_' + data.id + ' /><span>' + data.task + '</span><br />')
+                $('#todos-area').append('<p class="unchecked"><input type="checkbox" class="todo-check" id=todo_' + data.id + ' /><span>' + data.task + '</span></p>')
                 $('#remain').text($('#todos-area input:checkbox').length-$('#todos-area input:checkbox:checked').length)
                 $('.todo-check').on('click', function(e){
                     var isChecked = $(this).is(':checked')
